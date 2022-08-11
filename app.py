07b7b46e9eb42f7f7d430cb78637c502c5a237dd
Amanuel Bulbula <61360205+amanuelb898@users.noreply.github.com>
2022-08-11 15:09:33 -0400

    update app

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import nltk
 import collections
 from collections import Counter
 
-# genius = lyricsgenius.Genius("-5FNOZho_II0RPhtTdHiP9hXpKXrLBXx-20NCQaenJq_FXqggK8mQy6tayKFD9Ru")
+genius = lyricsgenius.Genius("-5FNOZho_II0RPhtTdHiP9hXpKXrLBXx-20NCQaenJq_FXqggK8mQy6tayKFD9Ru")
 
-# artist = genius.search_artist("Steve Lacy", max_songs=1, sort="title")
-# song = artist.song("Bad Habit")
-# ##print(song.lyrics)
-# artist.add_song(song)
+artist = genius.search_artist("Steve Lacy", max_songs=1, sort="title")
+song = artist.song("Bad Habit")
+songL = song.lyrics
+print(songL)
