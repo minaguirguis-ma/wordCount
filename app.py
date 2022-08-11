3213dc847a0c55a9cb29318d176652b8d653fb5e
Amanuel Bulbula <MY_NAME@example.comAmanuel.Bulbula@moodys.com>
2022-08-11 14:01:07 -0400

    added song

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 #Initial Commit
+import lyricsgenius
+
+genius = lyricsgenius.Genius("-5FNOZho_II0RPhtTdHiP9hXpKXrLBXx-20NCQaenJq_FXqggK8mQy6tayKFD9Ru")
+
+artist = genius.search_artist("Steve Lacy", max_songs=1, sort="title")
+
+print(artist.songs)
